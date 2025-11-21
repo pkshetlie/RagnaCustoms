737124bba827b2291d9df20840077edfca0bda6d
Rhokapa <pierrick.pobelle@gmail.com>
2025-11-21 22:16:26 +0100

    cleanup

--- src/Service/SongDifficultyNotationService.php
+++ src/Service/SongDifficultyNotationService.php
@@ -13,22 +13,12 @@ use App\Repository\SongDifficultyNotationRepository;
 
 class SongDifficultyNotationService
 {
-    public function __construct(
-        private readonly SongDifficultyNotationRepository $songDifficultyNotationRepository,
-        private readonly ScoreHistoryRepository $scoreHistoryRepository,
-    ) {
+    public function __construct(private readonly SongDifficultyNotationRepository $songDifficultyNotationRepository)
+    {
     }
 
     public function setScore(SongDifficulty $songDifficulty, ?Utilisateur $utilisateur, int $note)
     {
-        if ($utilisateur === null) {
-            return false;
-        }
-
-        if ($this->isAllowedToNote($songDifficulty, $utilisateur) === false) {
-            return false;
-        }
-
         $notation = $this->songDifficultyNotationRepository->findOneBy([
             'songDifficulty' => $songDifficulty,
             'user' => $utilisateur,
@@ -44,76 +34,6 @@ class SongDifficultyNotationService
         $this->songDifficultyNotationRepository->add($notation);
 
         return true;
-    }
-
-    public function isAllowedToNote(SongDifficulty $songDifficulty, ?Utilisateur $utilisateur): bool
-    {
-        if ($utilisateur === null) {
-            return false;
-        }
-
-        $scoreHistory = $this->scoreHistoryRepository->findOneBy([
-            'songDifficulty' => $songDifficulty,
-            'user' => $utilisateur,
-        ]);
-
-        if ($scoreHistory !== null) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function getCommunityNotations(Song $song): array
-    {
-        $difficultyNotations = [];
-
-        foreach ($song->getSongDifficulties() as $difficulty) {
-            $notation = $this
-                ->songDifficultyNotationRepository
-                ->createQueryBuilder('sdn')
-                ->select('SUM(sdn.note) AS total')
-                ->addSelect('COUNT(sdn.id) AS count')
-                ->where('sdn.songDifficulty = :difficulty')
-                ->setParameter('difficulty', $difficulty)
-                ->getQuery()->getOneOrNullResult();
-
-            if (!$notation || $notation['count'] < 1) {
-                $difficultyNotations[$difficulty->getDifficultyRank()->getLevel()] = [
-                    'difficulty' => $difficulty,
-                    'level' => $difficulty->getDifficultyRank()->getLevel()
-                ];
-                continue;
-            }
-
-            $difficultyNotations[$difficulty->getDifficultyRank()->getLevel()] =
-                [
-                    'difficulty' => $difficulty,
-                    'level' => $notation['total'] / $notation['count'],
-                ];
-        }
-
-        return $difficultyNotations;
-    }
-
-    public function getAllowedNotation(Song $song, ?Utilisateur $user): array
-    {
-        if ($user === null) {
-            return [];
-        }
-
-        $notes = [];
-
-        foreach ($song->getSongDifficulties() as $difficulty) {
-            if ($this->isAllowedToNote($difficulty, $user)) {
-                $notes[] = [
-                    'difficulty' => $difficulty,
-                    'note' => $this->getUserCommunityNote($difficulty, $user),
-                ];
-            }
-        }
-
-        return $notes;
     }
 
     public function getUserCommunityNote(mixed $difficulty, ?Utilisateur $user)
