9ee6e26b9c3a6dbbeeed85f30b47892d5b640bcb
Rhokapa <pierrick.pobelle@gmail.com>
2025-11-24 08:23:35 +0100

    fix it ralph

--- src/Entity/SongDifficulty.php
+++ src/Entity/SongDifficulty.php
@@ -80,7 +80,7 @@ class SongDifficulty
     /**
      * @var Collection<int, SongDifficultyNotation>
      */
-    #[ORM\OneToMany(mappedBy: 'songDifficultyNotation', targetEntity: SongDifficultyNotation::class, orphanRemoval: true)]
+    #[ORM\OneToMany(mappedBy: 'songDifficultyNotation', targetEntity: SongDifficultyNotation::class, orphanRemoval: true, cascade: ['persist', 'remove'])]
     private Collection $songDifficultyNotations;
 
     // /**
