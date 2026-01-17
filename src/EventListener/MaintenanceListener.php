0f3f102886f349d92808d8cfddbe11a2e6e3ab9f
Rhokapa <pierrick.pobelle@gmail.com>
2026-01-18 00:53:50 +0100

    try to kill api

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -2,18 +2,20 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class MaintenanceListener
 {
+    #[AsEventListener(event: 'kernel.request')]
     public function onKernelRequest(RequestEvent $event)
     {
         $request = $event->getRequest();
 
         // On vérifie si la route commence par /api
-        if (str_starts_with($request->getPathInfo(), '/api')) {
+        if (str_starts_with($request->getPathInfo(), '/api') || str_starts_with($request->getPathInfo(), '/rest-api')) {
             $event->setResponse(
                 new JsonResponse(['message' => 'API temporairement en maintenance'], Response::HTTP_SERVICE_UNAVAILABLE)
             );
